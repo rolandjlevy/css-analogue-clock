6c904082605df8eb4d9dcc5b6d238de64aba1b39
Roland Levy <rolandjlevy@gmail.com>
2020-06-20 14:18:21 +0000

    Added hour arm

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function setClockArms() {
-  const transition = 'all 0.3s ease'
+  const transition = 'all 0.3s ease';
   
-  const secondsArm = document.querySelector('.arm.seconds');
-  secondsArm.style.opacity = 1;
-  secondsArm.style.transition = transition;
+  initArm ('.arm.seconds', transition);
   const seconds = new Date().getSeconds();
   document.documentElement.style.setProperty('--delay-seconds', `-${seconds}s`);
 
-  const minutesArm = document.querySelector('.arm.minutes');
-  minutesArm.style.opacity = 1;
-  minutesArm.style.transition = transition;
+  initArm ('.arm.minutes', transition);
   const minutes = new Date().getMinutes();
   document.documentElement.style.setProperty('--delay-minutes', `-${minutes * 60 + seconds}s`);
+
+  initArm ('.arm.hours', transition);
+  const hours = new Date().getHours() % 12;
+  document.documentElement.style.setProperty('--delay-hours', `-${hours * 3600 + (minutes * 60) + seconds}s`);
+}
+
+function initArm (className, transition) {
+  const arm = document.querySelector(className);
+  arm.style.opacity = 1;
+  arm.style.transition = transition;
 }
