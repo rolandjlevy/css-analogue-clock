1f04a58ad51cc4c20ef91ccbcc8a620c051c133e
Roland Levy <rolandjlevy@gmail.com>
2020-06-21 00:40:23 +0000

    Tried to implement a reset timer button

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-  setClockArms();
+  initClockArms();
+  setCorrectTime();
+  initButtons();
 });
 
-function setClockArms() {
+function initClockArms() {
+  initArm('.arm.seconds');
+  initArm('.arm.minutes');
+  initArm('.arm.hours');
+}
+
+function initArm (className) {
+  const arm = document.querySelector(className);
   const transition = 'all 0.3s ease';
-  
-  initArm ('.arm.seconds', transition);
+  arm.classList.add('animate');
+  arm.style.opacity = 1;
+  arm.style.transition = transition;
+}
+
+function setCorrectTime() {
   const seconds = new Date().getSeconds();
   document.documentElement.style.setProperty('--delay-seconds', `-${seconds}s`);
-
-  initArm ('.arm.minutes', transition);
   const minutes = new Date().getMinutes();
   document.documentElement.style.setProperty('--delay-minutes', `-${minutes * 60 + seconds}s`);
-
-  initArm ('.arm.hours', transition);
   const hours = new Date().getHours() % 12;
   document.documentElement.style.setProperty('--delay-hours', `-${hours * 3600 + (minutes * 60) + seconds}s`);
 }
 
-function initArm (className, transition) {
-  const arm = document.querySelector(className);
-  arm.style.opacity = 1;
-  arm.style.transition = transition;
+function resetTimer() {
+  document.documentElement.style.setProperty('--delay-seconds', 0);
+  document.documentElement.style.setProperty('--delay-minutes', 0);
+  document.documentElement.style.setProperty('--delay-hours', 0);
+}
+
+function initButtons() {
+  const timeBtn = document.querySelector('.btn.time');
+  timeBtn.addEventListener('click', (e) => {
+    setCorrectTime();
+  });
+  const resetBtn = document.querySelector('.btn.reset');
+  resetBtn.addEventListener('click', (e) => {
+    resetTimer();
+  });
 }
