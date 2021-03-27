e3bac0df8aab94f100f263b69cbdc929c965e5f2
Roland Levy <contact@rolandlevy.co.uk>
2021-03-27 05:41:23 +0000

    Implemented IIFE

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const $ = (selector) => document.querySelector(selector);
 const setVar = (varName, value) => document.documentElement.style.setProperty(varName, value);
 $('.this-year').textContent = new Date().getFullYear();
 
-function setCorrectTime() {
+(() => {
   const seconds = new Date().getSeconds();
   setVar('--delay-seconds', `-${seconds}s`);
   const minutes = new Date().getMinutes();
   setVar('--delay-minutes', `-${minutes * 60 + seconds}s`);
   const hours = new Date().getHours() % 12;
   setVar('--delay-hours', `-${hours * 3600 + (minutes * 60) + seconds}s`);
-}
-
-setCorrectTime();
+})()
